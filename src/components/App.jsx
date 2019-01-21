364f35fbd716cce234c6c11d2829a7725868e1f0
Jonathan Mitzman <jonathan.mitzman@gmail.com>
2019-01-21 11:39:05 -0800

    "Updated form submittal and setState functionality"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
+import { EventEmitter } from 'events';
+
 class App extends React.Component {
     constructor (props) {
-        super(props) //I dont think any props will be required? TBD
+        super(props); //I dont think any props will be required? TBD
+        this.handleSubmit = this.handleSubmit.bind(this); //forever binds this to the App instance.
+
+        this.state = {
+            username: 'default',
+            message: 'default'
+        }
     }
-    AJAXclicker () {
-        console.log("click");
+
+    handleSubmit (event) {
+        /* Input: username and text
+         * Output: an AJAX post command
+         * Side-effect: needs to display the AJAX response on the screen
+         * Detailed steps TBD. Want to load JQUERY successfully.
+         */
+        event.preventDefault(); //stops HTML from being submitted
+        console.log(event.target);
+        this.setState({
+            username: 'update'/*something*/,
+            message: 'update'/*something*/
+        })
+
+        let messageData = {
+            username: this.state.username,
+            message: this.state.message
+        }
+
+        console.log((messageData));
+        /*
+        $.ajax({
+            url: 'http://ec2-13-57-25-101.us-west-1.compute.amazonaws.com:3000/api/hrsf110/greeting',
+            type: 'POST',
+            data: messageData,
+            contentType: 'application/json',
+            success: (success) => {console.log(`Options Success: ${JSON.stringify(success)}`)},
+            error: (error) => {console.log(`OPTIONS error: ${JSON.stringify(error)}`)}
+        })
+        */
+
     }
     render () {
-        return ( //the code below should eventually render a few forms
+        return (
             <div>
-                  <div><h5> Welcome to the AJAX User Interface! Exciting! </h5></div>
-                  <div>Please enter your name below</div>
-                  <input name="username" type="text"></input>
-                  <div>Please enter your message below</div>
-                  <input name="message" type="text"></input>
-                  <div>When you are ready, hit the submit button below</div>
-                  <button type="button" onClick={() => this.AJAXclicker()}>Submit AJAX request</button>
+                <div><h5> Welcome to the AJAX User Interface! Exciting! </h5></div>
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor='username'>Enter Username  </label>
+                    <input id='username' name='username' type='text' />
+                    <br></br>
+                    <label htmlFor='message'>Enter Message  </label>
+                    <input id='message' name='text' type='text' />
+                    <br></br>
+                    <button>Send AJAX Request</button>
+                </form>
+                    <div>AJAX Response below</div>
+                    <div id="AJAX">TBD</div>
             </div>
           )
     }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import App from './components/App.jsx';
 import React from 'React';
 import ReactDOM from 'react-dom';
+import jquery from 'jquery';
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
